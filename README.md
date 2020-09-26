3182d3d1d6c242afa308c8f73a2fd044e142d9e8
Allen <allen44@gmail.com@users.noreply.github.com>
2020-09-25 14:47:56 -1000

    updated readme, added randomize button

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # simple-js-gradient-generator
 A pure JS implementation of a simple CSS gradient generator
+
+"This is my... `css gradient generator`... There are many like it, but this one is mine."
--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector("#color1");
 var color2 = document.querySelector("#color2");
 var body = document.querySelector("#gradient");
+var randomColorsButton = document.querySelector("#random");
 
 function setGradient() {
 	body.style.background = ( "linear-gradient(to right, " 
@@ -12,8 +13,22 @@ function setGradient() {
 	css.textContent = body.style.background + ";";
 }
 
+function randomizeColors() {
+	color1.value = "#"+((1<<24)*Math.random()|0).toString(16);
+	color2.value = "#"+((1<<24)*Math.random()|0).toString(16);
+	setGradient();
+}
+
+
 color1.value = "#ff0000"; //Match background upon loading page
 color2.value = "#ffff00";
 
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
+
+// Display initial css on page load
+css.textContent = "background: linear-gradient(to right, red , yellow);"
+
+// on button click, set the two colors to random.
+randomColorsButton.addEventListener('click', randomizeColors);
+
